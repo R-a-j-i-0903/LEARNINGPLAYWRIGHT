55de04bcbff4682a6381b1de2c2998fae4189726
R-a-j-i-0903 <rajeswari231200@gmail.com>
2026-05-10 16:27:30 +0530

    Add Chapter_02_Javascript_Concepts and chapter_03_Identifier_Literals folders

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,14 @@
+var $ = 10;
+var _a = 12;
+var p = 10;
+
+var ab123 = 34;
+
+// var 123 = 123;
+
+var Name = "Rajeswari";
+var name = "Balla";
+//var rajeswari balla = "hello";
+var raji_balla = "hello";
+var raji$balla = "hello";
+var raji123 = "hello";
---
+++ chapter_03_Identifier_Literals/08_Comments.js
@@ -0,0 +1,37 @@
+// This is sinle comment this will be ignore 
+// this line will be not executed
+
+
+/*
+ *  This is multi line
+ *  Author : Rajeswari Balla
+ *  Date : 10-05-2026
+ */
+
+/**
+ *  This is multi line
+ *  Author : Rajeswari Balla
+ *  Date : 10-05-2026
+ **/
+
+// var g = 10;
+
+// this adjasdasdsa
+// this adjasdasdsa
+// this adjasdasdsa
+// this adjasdasdsa
+// this              adjasdasdsa
+// this adjasdasdsa
+// this adjasdasdsa
+//          this adjasdasdsa
+// this adjasdasdsa
+// this adjasdasdsa
+//           this adjasdasdsa
+// this adjasdasdsa
+// this         adjasdasdsa
+// this adjasdasdsa
+// this adjasdasdsa
+// this          adjasdasdsa
+// this adjasdasdsa
+
+var a = 10;
